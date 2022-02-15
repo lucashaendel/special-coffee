8d114a10bd126ed878a6222352d7f6e32a1bb5de
lucas haendel <lucas.ph@live.com>
2022-02-15 01:00:59 -0300

    modifico toda la pag

--- index.js
+++ index.js
@@ -20,3 +20,43 @@ hamburger.addEventListener('click', ()=>{
   barras.forEach(child => {child.classList.toggle('animado')});
 
 });
+
+
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.carrousel__lista'), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // draggable: true,
+    duration: 2,
+    dots: '.carrousel__indicadores',
+    arrows: {
+      prev: '.btn-previous',
+      next: '.btn-next'
+    },
+    responsive: [
+      {
+        // screens greater than >= 775px
+        breakpoint: 800,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: '2',
+          slidesToScroll: '2',
+          itemWidth: 150,
+          duration: 2
+          
+        }
+      },{
+        // screens greater than >= 1024px
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          itemWidth: 150,
+          duration: 2
+        }
+      }
+    ]
+
+  });
+
+});
